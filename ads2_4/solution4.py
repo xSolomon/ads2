c216d7b5967cf6e3fde4b46b4f0f5caf7bca1b4d
xSolomon <12345procto@gmail.com>
2024-09-23 12:24:01 +0700

    add lesson 4 solution

--- ads2_4/solution4.py
+++ ads2_4/solution4.py
@@ -8,11 +8,23 @@ class aBST:
         tree_size : int = pow(2, depth + 1) - 1
         self.TreeDepth : int = depth # Store depth to simplify work with tree.
         self.Tree = [None] * tree_size # Array of keys (tree modes).
-	
-    def FindKeyIndex(self, key):
-        # ищем в массиве индекс ключа
-        return None # не найден
-	
+
+    def FindKeyIndex(self, key) -> int | None:
+        ''' Returns key index if it was found.
+            Returns key index * (-1) if no key found, but there is valid place for it.
+            If no valid space, returns None. '''
+        current_node_index : int = 0
+        for i in range(self.TreeDepth + 1):
+            if self.Tree[current_node_index] is None:
+                return -current_node_index
+            if self.Tree[current_node_index] == key:
+                return current_node_index
+            if self.Tree[current_node_index] > key:
+                current_node_index = 2 * i + 1
+                continue
+            current_node_index = 2 * i + 2
+        return None
+
     def AddKey(self, key) -> int:
         ''' Adds key in tree and return its index, -1 if failed adding. '''
         current_node_index : int = 0
@@ -21,8 +33,8 @@ class aBST:
                 self.Tree[current_node_index] = key
                 return current_node_index
             if self.Tree[current_node_index] > key:
-                    current_node_index = 2 * i + 1
-                    continue
+                current_node_index = 2 * i + 1
+                continue
             current_node_index = 2 * i + 2
         return -1
 
