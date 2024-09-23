f854fe036577ee6ae019446fbdf2ae84312a77c8
xSolomon <12345procto@gmail.com>
2024-09-23 13:21:48 +0700

    add lesson 4 solution

--- ads2_4/solution4.py
+++ ads2_4/solution4.py
@@ -20,23 +20,22 @@ class aBST:
             if self.Tree[current_node_index] == key:
                 return current_node_index
             if self.Tree[current_node_index] > key:
-                current_node_index = 2 * i + 1
+                current_node_index = 2 * current_node_index + 1
                 continue
-            current_node_index = 2 * i + 2
+            current_node_index = 2 * current_node_index + 2
         return None
 
     def AddKey(self, key) -> int:
         ''' Adds key in tree and return its index, -1 if failed adding. '''
-        current_node_index : int = 0
-        for i in range(self.TreeDepth + 1):
-            if self.Tree[current_node_index] is None or self.Tree[current_node_index] == key:
-                self.Tree[current_node_index] = key
-                return current_node_index
-            if self.Tree[current_node_index] > key:
-                current_node_index = 2 * i + 1
-                continue
-            current_node_index = 2 * i + 2
-        return -1
+        add_key_index : int | None = self.FindKeyIndex(key)
+        if add_key_index is None or add_key_index > 0:
+            return -1
+        if add_key_index == 0 and self.Tree[0] is not None:
+            return -1
+        add_key_index *= -1
+        self.Tree[add_key_index] = key
+        return add_key_index
+
 
 
 
---
+++ ads2_4/tests4.py
@@ -0,0 +1,31 @@
+''' Tests for BST via array methods. '''
+
+import unittest
+from solution4 import aBST
+
+class aBSTTests(unittest.TestCase):
+    ''' Tests for aBST FindKeyIndex and Addkey methods. '''
+    def test_on_empty_tree(self) -> None:
+        ''' Expected find to give root key (0).
+            Also add must be root. '''
+        tree : aBST = aBST(0)
+        self.assertEqual(tree.FindKeyIndex(123), 0)
+        self.assertEqual(tree.AddKey(123), 0)
+        self.assertIsNotNone(tree.Tree[0])
+        self.assertEqual(tree.FindKeyIndex(123), 0)
+
+    def test_on_predefined_tree(self) -> None:
+        ''' Node values (in insert order) are:
+            50, 25, 75, 37, 62, 84, 31, 43, 55, 92
+            Expected correct BST form. '''
+        tree : aBST = aBST(3)
+        values : list[int] = [50, 25, 75, 37, 62, 84, 31, 43, 55, 92]
+        indexes : list[int] = [0, 1, 2, 4, 5, 6, 9, 10, 11, 14]
+        for i, key in enumerate(values):
+            with self.subTest(i = i, key = key, index = indexes[i]):
+                self.assertEqual(tree.AddKey(key), indexes[i])
+        for i, key in enumerate(values):
+            with self.subTest(i = i, key = key, index = indexes[i]):
+                self.assertEqual(tree.FindKeyIndex(key), indexes[i])
+
+unittest.main()
